911a484a90e3f4e77b85b6e6e41c694c620ee7c8
Takahiro Morimoto <tera09@me.com>
2014-11-18 17:01:14 +0900

    グラフ作れなかった

--- header.php
+++ header.php
@@ -15,31 +15,9 @@
         <link rel="stylesheet" href="/css/bootstrap.css" type="text/css">
         <link rel="stylesheet" href="/css/bootstrap-theme.css" type="text/css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script src="/js/bootstrap-ajax-typeahead/js/bootstrap-typeahead.js"></script>
+        <script src="/plugins/bootstrap-ajax-typeahead/js/bootstrap-typeahead.js"></script>
+
         <title>index</title>
-        <style type="text/css">
-            body{
-                padding-top:50px;
-            }
-            .instance_box{
-                height:150px;
-                background:#faebcc;
-            }
-            #information{
-                height:200px;
-                margin-top:10px;
-                margin-bottom:10px;
-                background:#bce8f1;
-            }
-            #navigation{
-                padding-top:10px;
-                padding-bottom:10px;
-                background:#d58512;
-            }
-            #instancelist{
-                margin-top:10px;
-            }
-        </style>
     </head>
     <body>
 <?php
--- index.php
+++ index.php
@@ -1,13 +1,49 @@
 <?php
 require 'header.php';
 ?>
+<style type="text/css">
+    body{
+        padding-top:50px;
+    }
+    .instance_box{
+        height:150px;
+        background:#faebcc;
+    }
+    #information{
+        height:200px;
+        /*margin-top:10px;*/
+        /*margin-bottom:10px;*/
+        background:#bce8f1;
+    }
+    #navigation{
+        padding-top:10px;
+        padding-bottom:10px;
+        background:#d58512;
+    }
+    #instancelist{
+        /*margin-top:10px;*/
+    }
+    .chart {
+        /*position: relative;*/
+        display: inline-block;
+        /*width: 11;*/
+        /*height: 110px;*/
+        margin-top: 50px;
+        margin-bottom: 50px;
+        text-align: center;
+    }
+    .chart canvas {
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+</style>
 <div class="container">
    <div class="row">
        <div class="col-xs-10 col-xs-offset-1">
             <!-- Main -->
            <div class="row">
-               <div class="col-xs-12" id="information">
-                    info
+                   info
                </div>
            </div>
            <div class="row">
@@ -97,5 +133,13 @@ require 'header.php';
 <div data-spy="affix" data-offset-top="60" data-offset-bottom="200">
     ...
 </div>
+<script type="text/javascript">
+    $('.chart').easyPieChart({
+        easing: 'easeOutBounce',
+        onStep: function(from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent));
+        }
+    });
+</script>
 </body>
 </html>
